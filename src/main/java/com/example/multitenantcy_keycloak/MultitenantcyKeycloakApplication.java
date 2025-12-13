f8dde0fb86966ed070b3389586ff3112b85e438c
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2025-12-13 17:17:11 +0100

    Create Spring boot app

---
+++ src/main/java/com/example/multitenantcy_keycloak/MultitenantcyKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.example.multitenantcy_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenantcyKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitenantcyKeycloakApplication.class, args);
+	}
+
+}
