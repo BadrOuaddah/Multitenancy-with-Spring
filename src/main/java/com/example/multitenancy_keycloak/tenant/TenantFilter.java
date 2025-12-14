3c2e26555532e78b58ce49002725db1e9c138bdf
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2025-12-14 16:07:39 +0100

    Create tenant

---
+++ src/main/java/com/example/multitenancy_keycloak/tenant/TenantFilter.java
@@ -0,0 +1,33 @@
+package com.example.multitenancy_keycloak.tenant;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class TenantFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tenant = request.getHeader("X-Tenant-ID");
+
+        if (tenant == null || tenant.isBlank()) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().write("X-Tenant-ID header is required");
+            return;
+        }
+
+        TenantContext.setTenant(tenant);
+
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            TenantContext.clear();
+        }
+    }
+
+}
