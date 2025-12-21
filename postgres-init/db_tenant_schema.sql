6c6d2bc3be5c5016e4d2e8988ded6052db3cbbea
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2025-12-22 00:09:34 +0100

    Update postgresql initialization

---
+++ postgres-init/db_tenant_schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS product_entity (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
--- postgres-init/init.sql
+++ postgres-init/init.sql
@@ -1,2 +1,8 @@
 CREATE DATABASE db_tenant1;
 CREATE DATABASE db_tenant2;
+
+\c db_tenant1
+\i /docker-entrypoint-initdb.d/db_tenant_schema.sql
+
+\c db_tenant2
+\i /docker-entrypoint-initdb.d/db_tenant_schema.sql
