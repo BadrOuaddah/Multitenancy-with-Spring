92102846d00920b80a27f76d3ff0a0786c069baf
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2025-12-21 19:31:53 +0100

    Add test and mockito dependencies in pom.xml

--- src/test/java/com/example/multitenancy_keycloak/MultitenancyKeycloakApplicationTests.java
+++ src/test/java/com/example/multitenancy_keycloak/MultitenancyKeycloakApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MultitenancyKeycloakApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
